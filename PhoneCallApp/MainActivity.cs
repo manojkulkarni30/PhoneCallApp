0d0ff6e7fffaf11db9b9d509f594311dc3f0b659
Manoj Sunil Kulkarni <manoj.kulkarni.3011@gmail.com>
2018-07-28 12:48:32 +0530

    Created a broadcast receiver for detecting phone call events

--- PhoneCallApp/MainActivity.cs
+++ PhoneCallApp/MainActivity.cs
@@ -1,31 +1,36 @@
-﻿using System;
-using Android.App;
-using Android.Widget;
+﻿using Android.App;
+using Android.Content;
 using Android.OS;
+using Android.Runtime;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
+using Plugin.CurrentActivity;
+using Plugin.Permissions;
+using System;
+using System.Threading.Tasks;
 
 namespace PhoneCallApp
 {
-	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-	public class MainActivity : AppCompatActivity
-	{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
 
-		protected override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
 
-			SetContentView(Resource.Layout.activity_main);
-
-			Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
-			FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
-		}
 
-		public override bool OnCreateOptionsMenu(IMenu menu)
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
             return true;
@@ -44,10 +49,33 @@ namespace PhoneCallApp
 
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
-            View view = (View) sender;
+            View view = (View)sender;
             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
-	}
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+        public Task<bool> AlertAsync(Context context, string title, string message, string positiveButton, string negativeButton)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            using (var db = new Android.Support.V7.App.AlertDialog.Builder(context))
+            {
+                db.SetTitle(title);
+                db.SetMessage(message);
+                db.SetPositiveButton(positiveButton, (sender, args) => { tcs.TrySetResult(true); });
+                db.SetNegativeButton(negativeButton, (sender, args) => { tcs.TrySetResult(false); });
+                db.Show();
+            }
+
+            return tcs.Task;
+        }
+    }
 }
 
